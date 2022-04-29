6807796bcc3ee630e12acc80d5754e07c7e00287
Rey Perez <rey.perez2@upr.edu>
2022-04-29 18:53:14 -0400

    modifies encrypt function, adds decipher function

--- playfair.py
+++ playfair.py
@@ -33,10 +33,10 @@ def generate_matrix(keyword: string) -> list:
 
 #prints the matrix
 def print_matrix(matrix: list):
-    print("\nThis is the key matrix: ")
+    print("\nThis is the key matrix: \033[96m")
     for row in matrix:
         print(row)
-    print()
+    print("\033[0m")
 
 
 def get_usr_input(option: int):
@@ -51,19 +51,21 @@ def get_usr_input(option: int):
 def show_menu():
     ans = ""
     while ans != 0:
-        print("What do you want to do?")
-        print("1. encrypt a message\n2. decipher a message\n0. EXIT")
+        print("\nWhat do you want to do?")
+        print("1. ENCRYPT a message\n2. DECIPHER a message\n0. EXIT")
         ans = int(input("Enter option number: "))
         if ans == 1:
             kw, msg = get_usr_input(ans)
             encrypt(kw, msg)
+            break
         elif ans == 2:
             kw, msg = get_usr_input(ans)
             decipher(kw, msg)
+            break
         elif ans != 0:
             print("\n\033[93m"+ str(ans), "is not a valid input\033[0m")
-            print("What do you want to do?")
-            print("1. encrypt a message\n2. decipher a message\n0. EXIT")
+            print("\nWhat do you want to do?")
+            print("1. ENCRYPT a message\n2. DECIPHER a message\n0. EXIT")
             ans = int(input("Enter option number: "))
 
 def process_message_string(message:string):
@@ -73,13 +75,12 @@ def process_message_string(message:string):
         if char not in string.ascii_uppercase:
             message = message.replace(char, "")
 
-    #separate identical characters by X
+    #check pairs, add X between twin letters
     i = 0
     while i < len(message)-1:
         if message[i] == message[i+1]:
-            message = message[:i+1] + "X" + message[i+1:]
-            i+=1 #skip the new X
-        i+=1
+            message = message[:i+1] + 'X' + message[i+1:]
+        i+=2
     
     #adds 'Z' to odd length strings
     if len(message) %2 != 0:
@@ -91,31 +92,30 @@ def process_message_string(message:string):
 def encrypt(keyword:string, message:string):
     pf_matrix = generate_matrix(keyword)
     print_matrix(pf_matrix)
+
     message = process_message_string(message)
 
-    #separate message into character pairs
-    pairs = wrap(message, 2)
-    
-    encrypted_message = ""
-    for pair in pairs:
-        loc1 = get_location(pair[0],pf_matrix)
-        loc2 = get_location(pair[1],pf_matrix)
-        # print (pair[0], loc1) ------------------------------------------------------ print letter locations
-        # print (pair[1], loc2)
-        
-        if loc1[0] == loc2[0]: #letters in same row
+    i = 0
+    encrypted_message=""
+    while i < len(message)-1:
+        #get letter locations in matrix
+        loc1 = get_location(message[i],pf_matrix) 
+        loc2 = get_location(message[i+1],pf_matrix)
+
+        #check letter position and substitute
+        if loc1[0] == loc2[0]: #letters in same row 
             r1,c1 = loc1[0],loc1[1]+1
             r2,c2, = loc2[0],loc2[1]+1
-            #adjust indeces; 4 is the highest index in a 5x5 matrix
+            #adjust column indeces; 4 is the highest index in a 5x5 matrix
             if c1 > 4: 
                 c1=0
             if c2 > 4:
                 c2= 0
-            encrypted_message += pf_matrix[r1][c1] + pf_matrix[r2][c2]
+            encrypted_message += pf_matrix[r1][c1] + pf_matrix[r2][c2] #concat pair substitution
         elif loc1[1] == loc2[1]: #letters in same column
             r1,c1 = loc1[0]+1,loc1[1]
             r2,c2, = loc2[0]+1,loc2[1]
-            #adjust indices 
+            #adjust row indices 
             if r1 > 4: 
                 r1=0
             if r2 > 4:
@@ -125,9 +125,24 @@ def encrypt(keyword:string, message:string):
             r1,c1 = loc1[0],loc1[1]
             r2,c2, = loc2[0],loc2[1]
             encrypted_message += pf_matrix[r1][c2] + pf_matrix[r2][c1]
+        
+        i+=2 #next two letters
+    print("This is the encrypted message:\033[96m")
+    print_pairs(encrypted_message)
+    print("\033[0m")
 
-    print(encrypted_message)
+def print_pairs(str:string):
+    i=0
+    paired = ""
+    for letter in str:
+        if i >= 2:
+            paired += " "
+            i=0
+        paired += letter
+        i+=1
+    print(paired)
 
+#returns tuple of letter coordinates 
 def get_location(letter:string, matrix:list):
     row = 0
     for r in matrix:
@@ -143,7 +158,45 @@ def get_location(letter:string, matrix:list):
 #returns a string containing the plain text message
 def decipher(keyword:string, message:string):
     pf_matrix = generate_matrix(keyword)
-    #TODO 
+    print_matrix(pf_matrix)
+
+    message = message.replace(" ", "") #remove whitespace
+
+    i = 0
+    deciphered_message=""
+    while i < len(message)-1:
+        #get letter locations in matrix
+        loc1 = get_location(message[i],pf_matrix) 
+        loc2 = get_location(message[i+1],pf_matrix)
+
+        #check letter position and substitute
+        if loc1[0] == loc2[0]: #letters in same row 
+            r1,c1 = loc1[0],loc1[1]-1
+            r2,c2, = loc2[0],loc2[1]-1
+            #adjust column indeces; 4 is the highest index in a 5x5 matrix
+            if c1 < 0: 
+                c1 = 4
+            if c2 < 0:
+                c2 = 4
+            deciphered_message += pf_matrix[r1][c1] + pf_matrix[r2][c2] #concat substitution
+        elif loc1[1] == loc2[1]: #letters in same column
+            r1,c1 = loc1[0]-1,loc1[1]
+            r2,c2, = loc2[0]-1,loc2[1]
+            #adjust row indices 
+            if r1 < 0: 
+                r1 = 4
+            if r2 < 0:
+                r2 = 4
+            deciphered_message += pf_matrix[r1][c1] + pf_matrix[r2][c2]
+        else: #rectangle formed
+            r1,c1 = loc1[0],loc1[1]
+            r2,c2, = loc2[0],loc2[1]
+            deciphered_message += pf_matrix[r1][c2] + pf_matrix[r2][c1]
+        
+        i+=2 # evaluate next two letters
+    print("This is the deciphered message message:\033[96m")
+    print(deciphered_message)
+    print("\033[0m")
 
 def main():
     show_menu()
